41da050623a2f11bfa351bc05cbe1fb733fad64b
Miguel-David Orrico Teruel <otm@iesseveroochoa.net>
2018-11-11 18:37:06 +0100

    Clase Base

---
+++ src/PiramideInvertida.java
@@ -0,0 +1,11 @@
+public class PiramideInvertida {
+    public static void main(String[] args) {
+        int filas=5;
+        System.out.println(cadenaPiramideInvertida(filas));
+    }
+
+    private static String cadenaPiramideInvertida(int filas) {
+
+        return null;
+    }
+}
