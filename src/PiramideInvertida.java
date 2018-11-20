2d73778350e9c2083c171c05163cabc22647607c
Manuel Martinez <descom666999@gmail.com>
2018-11-20 11:46:31 +0100

    @migueldavidorrico terminada la piramide, tuve que consultar en internet

--- src/PiramideInvertida.java
+++ src/PiramideInvertida.java
@@ -5,7 +5,26 @@ public class PiramideInvertida {
     }
 
     public static String cadenaPiramideInvertida(int filas) {
+        String salida = "";
 
-        return null;
+        int ancho=(filas-1)*2+1;
+        int espaciosIniciales=0;
+
+        salida+="\n";
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < espaciosIniciales; j++) {
+                salida+=" ";
+            }
+            for (int j = 0; j < ancho; j++) {
+                salida+="*";
+            }
+            salida+="\n";
+            ancho=ancho-2;
+            espaciosIniciales++;
+        }
+        return salida;
     }
+
 }
+
